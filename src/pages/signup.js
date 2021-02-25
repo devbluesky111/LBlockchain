29ea0c9998fc5e6c4daa8c58a48c9d489c4423b3
devbluesky111 <talent.dev111@gmail.com>
2021-02-24 23:01:42 -0800

    register page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,39 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+// import { register } from './../redux/actions/auth';
 
-export default function signup() {
+const Signup = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    walletAddress: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const { name, walletAddress, password, confirmPassword } = formData;
+  const [termAgree, setTermAgree] = useState(true);
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+
+  useEffect(() => {
+    console.log('react hook here!!');
+  },[]);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      // setAlert('Passwords do not match', 'danger');
+      console.log('password don\'t match')
+    } else {
+      // register({ name, walletAddress, password });
+      // const res = await api.post('/users', {name, walletAddress, password});
+      console.log('submitted')
+    }
+  }
+
   return (
     <>
       <div className="vh-100 d-flex justify-content-center">
         <div className="form-access my-auto">
-          <form>
+          <form onSubmit={onSubmit}>
             <span>Create Account</span>
             <div className="form-group">
               <input
                 type="text"
-                className="form-control"
+                name="name"
+                className="form-control text-white"
                 placeholder="Full Name"
+                value={name}
+                onChange={onChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="text"
-                className="form-control"
+                name="walletAddress"
+                className="form-control text-white"
                 placeholder="Wallet Address"
+                value={walletAddress}
+                onChange={onChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="password"
-                className="form-control"
+                name="password"
+                className="form-control text-white"
                 placeholder="Password"
+                value={password}
+                onChange={onChange}
               />
             </div>
             <div className="form-group">
               <input
                 type="password"
-                className="form-control"
+                name="confirmPassword"
+                className="form-control text-white"
                 placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={onChange}
               />
             </div>
             <div className="custom-control custom-checkbox">
@@ -41,13 +84,15 @@ export default function signup() {
                 type="checkbox"
                 className="custom-control-input"
                 id="form-checkbox"
+                value={termAgree}
+                onChange={() => {setTermAgree(!termAgree)}}
               />
               <label className="custom-control-label" htmlFor="form-checkbox">
                 I agree to the{' '}
                 <Link to="/terms-and-conditions">Terms & Conditions</Link>
               </label>
             </div>
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" disabled={termAgree} >
               Create Account
             </button>
           </form>
@@ -60,3 +105,5 @@ export default function signup() {
     </>
   );
 }
+
+export default Signup;
