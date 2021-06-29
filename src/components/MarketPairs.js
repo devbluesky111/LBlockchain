69dae782252d876dddab71622b812eb9833a19d6
devbluesky111 <talent.dev111@gmail.com>
2021-06-29 06:59:05 -0700

    change responsive and styles

--- src/components/MarketPairs.js
+++ src/components/MarketPairs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
 
 export default function MarketPairs({changeContract, btc, eth, xrp, doge, link, ltc}) {
 
@@ -19,52 +20,40 @@ export default function MarketPairs({changeContract, btc, eth, xrp, doge, link, 
           />
         </div>
         <h5 className="market-pairs-header">Hyper Contract</h5>
-            <table className="table" size="sm">
+            <Table responsive style={{overflowX: 'scroll'}}>
               <tbody>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:BTCUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> BTC/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:BTCUSDT')}}>
+                  <td>BTC/USDT</td>
                   <td>{btc.askPrice}</td>
                   <td className={(btc.priceChangePercent > 0) ? `text-success` : `text-danger`}>{btc.priceChangePercent}%</td>
                 </tr>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:ETHUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> ETH/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:ETHUSDT')}}>
+                  <td>ETH/USDT</td>
                   <td>{eth.askPrice}</td>
                   <td className={(eth.priceChangePercent > 0) ? `text-success` : `text-danger`}>{eth.priceChangePercent}%</td>
                 </tr>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:XRPUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> XRP/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:XRPUSDT')}}>
+                  <td>XRP/USDT</td>
                   <td>{xrp.askPrice}</td>
                   <td className={(xrp.priceChangePercent > 0) ? `text-success` : `text-danger`}>{xrp.priceChangePercent}%</td>
                 </tr>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:DOGEUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> DOGE/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:DOGEUSDT')}}>
+                  <td>DOGE/USDT</td>
                   <td>{doge.askPrice}</td>
                   <td className={(doge.priceChangePercent > 0) ? `text-success` : `text-danger`}>{doge.priceChangePercent}%</td>
                 </tr>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:LINKUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> LINK/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:LINKUSDT')}}>
+                  <td>LINK/USDT</td>
                   <td>{link.askPrice}</td>
                   <td className={(link.priceChangePercent > 0) ? `text-success` : `text-danger`}>{link.priceChangePercent}%</td>
                 </tr>
-                <tr className="mt15" onClick={() => {changeContract('BINANCE:LTCUSD')}}>
-                  <td>
-                    <i className="icon ion-md-star"></i> LTC/USD
-                  </td>
+                <tr className="mt15" onClick={() => {changeContract('BINANCE:LTCUSDT')}}>
+                  <td>LTC/USDT</td>
                   <td>{ltc.askPrice}</td>
                   <td className={(ltc.priceChangePercent > 0) ? `text-success` : `text-danger`}>{ltc.priceChangePercent}%</td>
                 </tr>
               </tbody>
-            </table>
+            </Table>
       </div>
     </>
   );
--- src/components/MarketTrade.js
+++ src/components/MarketTrade.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Tabs, Tab } from 'react-bootstrap';
-import Backend from './../@utils/BackendUrl';
-import axios from 'axios';
+import swal from 'sweetalert';
 import PropTypes from 'prop-types';
 import { connect } from "react-redux";
-import { createContract } from "./../redux/actions/contractAction";
+import { createBuyContract } from "./../redux/actions/contractAction";
+import { createSellContract } from "./../redux/actions/contractAction";
 
-const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) => {
+const MarketTrade = ({ createBuyContract, createSellContract, symbol, btc, eth, xrp, doge, link, ltc, auth : {balance}}) => {
 
   const [orderValue, setOrderValue] = useState('');
   const [orderTime, setOrderTime] = useState('');
@@ -14,19 +14,19 @@ const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) 
 
   useEffect(() => {
     switch (symbol) {
-      case 'BINANCE:BTCUSD':
+      case 'BINANCE:BTCUSDT':
         setRate(btc);
         break;
-      case 'BINANCE:ETHUSD':
+      case 'BINANCE:ETHUSDT':
         setRate(eth);
         break;
-      case 'BINANCE:XRPUSD':
+      case 'BINANCE:XRPUSDT':
         setRate(xrp);
         break;
-      case 'BINANCE:DOGEUSD':
+      case 'BINANCE:DOGEUSDT':
         setRate(doge);
         break;
-      case 'BINANCE:LINKUSD':
+      case 'BINANCE:LINKUSDT':
         setRate(link);
         break;    
       default:
@@ -39,10 +39,18 @@ const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) 
     var orderIsValid = true;
     if(orderValue < 1) {
       orderIsValid = false;
-      alert('Margin must be greater than 1 USD')
+      swal({
+        text: "The order value must be greater than 1 USDT. Reenter the value",
+        icon: "warning",
+        dangerMode: true
+      })
     } else if(orderValue > 500) {
       orderIsValid = false;
-      alert('Max. Margin Allowed 500 USD')
+      swal({
+        text: "Max order value Allowed is 500 USDT. Reenter the value",
+        icon: "warning",
+        dangerMode: true
+      })
     }
     return orderIsValid;
   }
@@ -50,12 +58,12 @@ const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) 
   const orderBuy = async () => {
     if(orderValidation()){
       setTimeout(() => {
-        createContract({margin: orderValue, openingPrice: rate.askPrice, orderTime: orderTime, symbol: symbol});      
+        createBuyContract({margin: orderValue, openingPrice: rate.askPrice, orderTime: orderTime, symbol: symbol});      
       }, 1000);
       setOrderValue('');
       setOrderTime('');
       setRate({askPrice:'', priceChangePercent:''});
-    } else {      
+    } else {
       setOrderValue('');
       setOrderTime('');
       setRate({askPrice:'', priceChangePercent:''});
@@ -63,111 +71,106 @@ const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) 
   }
   
   const orderSell = async () => {
-    console.log('Sell button clicked');
-    const res = await axios.post(Backend.URL + '/api/contracts/order/close', {orderTime: orderTime, orderValue: orderValue, spreadRate: rate.askPrice}, {withCredentials: true, headers: {"Access-Control-Allow-Origin": "*"} });
-    console.log('sell==>', res.data);
+    if(orderValidation()){
+      setTimeout(() => {
+        createSellContract({margin: orderValue, openingPrice: rate.askPrice, orderTime: orderTime, symbol: symbol});      
+      }, 1000);
+      setOrderValue('');
+      setOrderTime('');
+      setRate({askPrice:'', priceChangePercent:''});
+    } else {
+      setOrderValue('');
+      setOrderTime('');
+      setRate({askPrice:'', priceChangePercent:''});
+    }
   }
   return (
     <>
       <div className="market-trade">
         <Tabs defaultActiveKey="trade">
           <Tab eventKey="trade" title="Trade">
-            <div className="d-flex justify-content-between">
-                <form action="#">
-                  <div className="form-row mt20">
-                    <select
-                      id="selectCurrency"
-                      className="custom-select"
-                      style={{textAlign:'center'}}
-                    >
-                      <option defaultValue>BNB Account</option>
-                      <option>BTC Account</option>
-                    </select>
-                  </div>
-                  <div className="form-row" style={{marginTop:'34px'}}>
-                    <label htmlFor="orderValue" className="text-white pl-2">Margin</label>
-                    <div className="input-group">
-                      <input
-                        type="number"
-                        id="orderValue"
-                        className="form-control"
-                        placeholder="Amount"
-                        value={orderValue}
-                        onChange={(e) => {setOrderValue(e.target.value)}}
-                      />
-                      <div className="input-group-append">
-                        <span className="input-group-text">USD</span>
-                      </div>
+              <form action="#">
+                <div className="form-row mt20">
+                  <select
+                    id="selectCurrency"
+                    className="custom-select"
+                    style={{textAlign:'center'}}
+                  >
+                    <option defaultValue>BNB Account</option>
+                    <option>BTC Account</option>
+                  </select>
+                </div>
+                <div className="form-row" style={{marginTop:'34px'}}>
+                  <label htmlFor="orderValue" className="text-white pl-2">Order Value</label>
+                  <div className="input-group">
+                    <input
+                      type="number"
+                      id="orderValue"
+                      className="form-control"
+                      placeholder="Amount"
+                      value={orderValue}
+                      onChange={(e) => {setOrderValue(e.target.value)}}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text">USDT</span>
                     </div>
                   </div>
-                  <div className="form-row mt20">
-                    <label htmlFor="orderTime" className="text-white pl-2">Order Time</label>
-                    <div className="input-group">
-                      <input
-                        type="number"
-                        id="orderTime"
-                        className="form-control"
-                        placeholder="Amount"
-                        value={orderTime}
-                        onChange={(e) => {setOrderTime(e.target.value)}}
-                      />
-                      <div className="input-group-append">
-                        <span className="input-group-text">Sec</span>
-                      </div>
+                </div>
+                <div className="form-row mt20">
+                  <label htmlFor="orderTime" className="text-white pl-2">Order Time</label>
+                  <div className="input-group">
+                    <input
+                      type="number"
+                      id="orderTime"
+                      className="form-control"
+                      placeholder="Amount"
+                      value={orderTime}
+                      onChange={(e) => {setOrderTime(e.target.value)}}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text">Sec</span>
                     </div>
                   </div>
-                  <div className="form-row mt20">
-                    <label htmlFor="availableValue" className="text-white pl-2">Available Value</label>
-                    <div className="input-group">
-                      <input
-                        type="text"
-                        id="availableValue"
-                        className="form-control"
-                        placeholder="Amount"
-                        readOnly
-                        value="5000"
-                      />
-                      <div className="input-group-append">
-                        <span className="input-group-text">USD</span>
-                      </div>
+                </div>
+                <div className="form-row mt20">
+                  <label htmlFor="availableValue" className="text-white pl-2">Available Value Per Trade</label>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      id="availableValue"
+                      className="form-control"
+                      placeholder="Amount"
+                      readOnly
+                      value={balance > 500 ? 500 : balance}
+                    />
+                    <div className="input-group-append">
+                      <span className="input-group-text">USDT</span>
                     </div>
                   </div>
-                  <div className="form-row mt20">
-                    <label htmlFor="currentRate" className="text-white pl-2">Current Price &nbsp;&nbsp; ( {symbol.split(':')[1]} )</label>
-                    <div className="input-group">
-                      <input
-                        type="text"
-                        id="currentRate"
-                        className="form-control"
-                        placeholder="Amount"
-                        readOnly
-                        value={rate.askPrice}
-                        required
-                      />
-                    </div>
+                </div>
+                <div className="form-row mt20">
+                  <label htmlFor="currentRate" className="text-white pl-2">Current Price &nbsp;&nbsp; ( {symbol.split(':')[1]} )</label>
+                  <div className="input-group">
+                    <input
+                      type="text"
+                      id="currentRate"
+                      className="form-control"
+                      placeholder="Amount"
+                      readOnly
+                      value={rate.askPrice}
+                      required
+                    />
                   </div>
-                  {/* <div className="form-row">
-                    <label className="text-white">Test</label>
-                    <div className="input-group">
-                      <input type="text" list="cars" />
-                      <datalist id="cars">
-                        <option>1 min</option>
-                        <option>5 min</option>
-                        <option>10 min</option>
-                        <option>20 min</option>
-                      </datalist>
-                    </div>
-                  </div> */}
-                  <div className="row mt15">
-                    <button type="submit" className="btn buy" onClick={()=> {orderBuy()}}>
-                      Buy
-                    </button>
-                    <button type="submit" className="btn sell" onClick={()=> {orderSell()}}>
-                      Sell
-                    </button>
-                  </div>                  
-                </form>
-            </div>
+                </div>
+                <div className="row mt15">
+                  <button type="submit" className="btn buy" onClick={()=> {orderBuy()}}>
+                    Buy
+                  </button>
+                  <button type="submit" className="btn sell" onClick={()=> {orderSell()}}>
+                    Sell
+                  </button>
+                </div>                  
+              </form>
           </Tab>
         </Tabs>
       </div>
@@ -176,11 +179,17 @@ const MarketTrade = ({ createContract, symbol, btc, eth, xrp, doge, link, ltc}) 
 }
 
 MarketTrade.propTypes = {
-  createContract: PropTypes.func.isRequired
+  createBuyContract: PropTypes.func.isRequired,
+  createSellContract: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
 };
 
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
 export default connect(
-  null, {createContract}
+  mapStateToProps, {createBuyContract, createSellContract}
   )(MarketTrade);
 
 
