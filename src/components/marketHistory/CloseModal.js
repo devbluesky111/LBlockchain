416ebf68c20aac529e04e54c542d07d38a937d83
devbluesky111 <talent.dev111@gmail.com>
2021-07-01 07:04:39 -0700

    Insert Real time data on close modal

--- src/components/marketHistory/CloseModal.js
+++ src/components/marketHistory/CloseModal.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button, Container, Col, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
 import { closeContract } from '../../redux/actions/contractAction';
+import { getContractHistory } from '../../redux/actions/contractAction';
 
-function CloseModal({ show, onHide, closeContract, contract: {contract, loading}}) {
-  const confirmClose = () => {
+function CloseModal({ show, onHide, btc, eth, xrp, doge, link, ltc, closeContract, getContractHistory, contract: {contract, loading}}) {
+
+  const [currentRate, setCurrentRate] = useState(null);
+
+  useEffect(() => {
+    if(contract !== null){
+      switch (contract.symbol) {
+        case 'BINANCE:BTCUSDT':
+          setCurrentRate(btc.askPrice);
+          break;
+        case 'BINANCE:ETHUSDT':
+          setCurrentRate(eth.askPrice);
+          break;
+        case 'BINANCE:XRPUSDT':
+          setCurrentRate(xrp.askPrice);
+          break;
+        case 'BINANCE:DOGEUSDT':
+          setCurrentRate(doge.askPrice);
+          break;
+        case 'BINANCE:LINKUSDT':
+          setCurrentRate(link.askPrice);
+          break;    
+        default:
+          setCurrentRate(ltc.askPrice);
+          break;
+      }
+    }
+  }, [contract, btc, eth, xrp, doge, link, ltc]);
+
+  const confirmClose = (data) => {
     setTimeout(() => {
-      closeContract(contract._id);
+      closeContract({id:data.id, orderValue:data.orderValue, closingPrice:data.closingPrice, profitLoss:data.profitLoss});
+      getContractHistory();
       onHide();
     }, 1000);
   }
@@ -23,42 +53,50 @@ function CloseModal({ show, onHide, closeContract, contract: {contract, loading}
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Close
-          </Modal.Title>
-      </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <Row className="mb-2">
-            <Col xs={12} md={8}>
-                {contract && contract.symbol.split(':')[1]}
-            </Col>
-            <Col xs={6} md={4}>
-            </Col>
-          </Row>
-          <Row className="mb-2">
-            <Col xs={12} md={8}>
-                Opening Price:
-            </Col>
-            <Col xs={6} md={4}>
-                {contract && contract.openingPrice}
-            </Col>
-          </Row> 
-          <Row>
-            <Col xs={12} md={8}>
-                Est. Closing Price:
-            </Col>
-            <Col xs={6} md={4}>
-                {contract && contract.openingPrice}
-            </Col>             
-          </Row>
-        </Container>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={onHide} className="closeButton">Cancel</Button>
-        <Button onClick={() => {confirmClose()}}>Confirm</Button>
-      </Modal.Footer>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Close
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="show-grid">
+          <Container>
+            <Row className="mb-2">
+              <Col xs={12} md={8}>
+                  {contract && contract.symbol.split(':')[1]}
+              </Col>
+              <Col xs={6} md={4}>
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col xs={12} md={8}>
+                  Unrealize PL Amount:
+              </Col>
+              <Col xs={6} md={4}>
+                  {contract && currentRate && ((currentRate - contract.openingPrice)/contract.openingPrice*contract.margin).toFixed(2)}
+              </Col>
+            </Row>
+            <Row className="mb-2">
+              <Col xs={12} md={8}>
+                  Opening Price:
+              </Col>
+              <Col xs={6} md={4}>
+                  {contract && contract.openingPrice}
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={12} md={8}>
+                  Est. Closing Price:
+              </Col>
+              <Col xs={6} md={4}>
+                  {currentRate}
+              </Col>             
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide} className="closeButton">Cancel</Button>
+          <Button onClick={() => {contract && currentRate && confirmClose({id:contract._id, orderValue:contract.margin, closingPrice:currentRate, profitLoss:((currentRate - contract.openingPrice)/contract.openingPrice*contract.margin).toFixed(2)})}}>Confirm</Button>
+        </Modal.Footer>
     </Modal>
   );
 }
@@ -71,4 +109,4 @@ const mapStateToProps = (state) => ({
     contract: state.contract
 });
 
-export default connect(mapStateToProps, {closeContract})(CloseModal);
+export default connect(mapStateToProps, {closeContract, getContractHistory})(CloseModal);
